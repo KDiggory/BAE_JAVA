bf8a401454a692293d35da005b2ccb1695628fb0
KDiggory <89535948+KDiggory@users.noreply.github.com>
2021-11-09 11:01:02 +0000

    Added another method to Person class

---
+++ Person.java
@@ -0,0 +1,41 @@
+package com.bae.oop;
+
+public class Person {
+	
+	private String name;
+	private float height;
+	private int shoeSize;
+	private int age;
+	private String jobTitle;
+	private String favouriteAnimal;
+	
+	public Person(String name, float height, int shoeSize, int age, String jobTitle, String favouriteAnimal) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.shoeSize = shoeSize;
+		this.age = age;
+		this.jobTitle = jobTitle;
+		this.favouriteAnimal = favouriteAnimal;
+	}
+
+	public Person(String name, float height) {
+		super();
+		this.name = name;
+		this.height = height;
+	}
+	
+	public void methodOne() {
+		System.out.println("Hello there " + name + ", I hear your favourite animal is the " + favouriteAnimal);
+		
+	}
+	
+	public void methodTwo() {
+		age = age +10;
+		System.out.println(name + ", In 10 years you will be " + age + " years old!!");
+	}
+	
+	public void methodThree() {
+		System.out.println(name + " Your job is: " + jobTitle);
+	}
+}
---
+++ PersonRunner.java
@@ -0,0 +1,22 @@
+package com.bae.oop;
+import com.bae.oop.Person;
+
+public class PersonRunner {
+
+	public static void main(String[] args) {
+		Person katie = new Person("Katie", 1.64f, 5, 33, "Software Engineer", "Ferret");
+		Person george = new Person("George", 1.84f, 10, 33, "GP", "Dragon");
+		Person huxley = new Person("Huxley", 0.98f, 7, 3, "Threenager", "Llama");
+		Person oswin = new Person("Oswin", 1.64f, 4, 1, "Cutie of the year", "Cat");
+		
+		katie.methodOne();
+		george.methodTwo();
+		huxley.methodOne();
+		oswin.methodTwo();
+		huxley.methodThree();
+		oswin.methodThree();
+	
+
+	}
+
+}
